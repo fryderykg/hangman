da71fc1fa6544f089abfa800facd8d800d9c86a4
FryderykG <eszkimos@gmail.com>
2017-01-18 12:17:47 +0100

    add sass, imagemin and watch configuration to grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,42 @@
-/**
- * Created by frycek on 18.01.17.
- */
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/main.css': 'style.sass'
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'pics/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'build/img/'
+                }]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['**/*.sass'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+    // Load the plugins tasks
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ["sass", "imagemin"]);
+};
