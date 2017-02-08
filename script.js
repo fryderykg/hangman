4725681d7fb6e1d71c06c4e44a16f2a40bae28aa
FryderykG <eszkimos@gmail.com>
2017-02-08 16:37:58 +0100

    add modal with rules add style and logic to display rules

--- script.js
+++ script.js
@@ -323,3 +323,36 @@ function start() {
     // EVENT LISTENERS
     addListeners();
 }
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("btnHelp");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    // modal.style.display = "block";
+    modal.style.opacity = "1";
+    modal.style.zIndex = "10";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    // modal.style.display = "none";
+    modal.style.opacity = "0";
+    modal.style.zIndex = "-10";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        // modal.style.display = "none";
+        modal.style.opacity = "0";
+        modal.style.zIndex = "-10";
+    }
+};
